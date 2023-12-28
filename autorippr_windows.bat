f376fc318ac89b01d194f98130c3b810cf58f29e
Luis Finke <luisfinke@gmail.com>
2023-12-28 12:56:15 -0500

    auto-install requirements, auto-create settings.cfg, remove log and add comments

--- autorippr_windows.bat
+++ autorippr_windows.bat
@@ -1,14 +1,24 @@
 @ECHO OFF
 setlocal
 
+:: get current directory path, removing trailing slash if needed
 set CURRENT_SCRIPT_DIR=%~dp0
-IF %CURRENT_SCRIPT_DIR:~-1%==\ (
-	SET CURRENT_SCRIPT_DIR=%CURRENT_SCRIPT_DIR:~0,-1%
+if %CURRENT_SCRIPT_DIR:~-1%==\ (
+	set CURRENT_SCRIPT_DIR=%CURRENT_SCRIPT_DIR:~0,-1%
 )
 
-set PYTHONPATH=%CURRENT_SCRIPT_DIR%\classes;%PYTHONPATH%
-echo %PYTHONPATH%
+:: install requirements
+pip install -r "%CURRENT_SCRIPT_DIR%\requirements.txt"
 
+:: create settings.cfg if missing
+if not exist "%CURRENT_SCRIPT_DIR%\settings.cfg" (
+	copy "%CURRENT_SCRIPT_DIR%\settings.example.cfg" "%CURRENT_SCRIPT_DIR%\settings.cfg"
+)
+
+:: update PYTHONPATH to include classes folder
+set PYTHONPATH=%CURRENT_SCRIPT_DIR%\classes;%PYTHONPATH%
+
+:: run autorippr
 python autorippr.py %*
 
 endlocal
